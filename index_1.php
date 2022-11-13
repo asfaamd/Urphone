547ca45b65708cf9f0ded68021c010262e553fc4
DESKTOP-7O4KFJO\Asfa Amalia Dinata <asfaamalia23@gmail.com>
2022-11-13 15:43:45 +0700

    no message

--- index_1.php
+++ index_1.php
@@ -12,7 +12,7 @@
   <div class="container-fluid">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link active" href="#">Active</a>
+        <a class="nav-link active" href="#">Lalala yeyeye</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
